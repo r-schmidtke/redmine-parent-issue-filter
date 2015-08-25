c36b2e6f90fbbe62de02f5a5ee293e381eaed660
r-schmidtke <robin@schmidtke-ct.de>
2015-08-25 12:48:13 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,9 @@
-require_dependency 'redmine_parent_issue_filter'
+require_dependency 'redmine_parent_issue_filter_rs'
 
-Redmine::Plugin.register :parent_issue_filter do
+Redmine::Plugin.register :parent_issue_filter_rs do
   name 'Parent issue filter plugin'
-  author 'onozaty'
+  author 'Robin Schmidtke'
   description 'Parent issue filter plugin for Redmine'
-  version '1.0.2'
-  url 'https://github.com/onozaty/redmine-parent-issue-filter'
-  author_url 'https://github.com/onozaty'
+  version '1.0.3'
+  url 'https://github.com/r-schmidtke/redmine-parent-issue-filter-rs'
 end
